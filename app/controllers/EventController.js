2838671acac8ad13e181701473dba4cc0841c655
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2015-12-21 21:16:21 +0700

    Working on about ng-repeat

--- app/controllers/EventController.js
+++ app/controllers/EventController.js
@@ -2,8 +2,25 @@
 
 eventsApp.controller('EventController', function($scope) {
 	$scope.event = {
-		name: 'Angular Name',
+		name: 'Android Name',
 		date: '21/12/58',
-		time: '10:30 pm'
+		time: '10:30 pm',
+		location: {
+			address: 'Google Headquarters',
+			city: 'Mountain View',
+			province: 'CA'
+		},
+		imageUrl: 'images/angularjs-logo.png',
+		sessions: [
+			{
+				name: 'Directive Masterclass'
+			},
+			{
+				name: 'Scopes for fun and profit'
+			},
+			{
+				name: 'Well Behaved Controllers' 
+			}
+		]
 	};
 });
