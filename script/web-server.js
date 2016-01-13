685b3191ef833121e86b5139a2188d0b0c92289e
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2016-01-13 19:40:12 +0700

    Updating a Node Server for JSON Requests - Chapter 3.6

---
+++ script/web-server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var events = require('./eventsController');
+var app = express();
+var rootpath = path.normalize(__dirname + '/../');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(rootpath));
+//app.use(express.static(__dirname + '/'));
+
+app.get('/data/event/:id', events.get);
+app.post('/data/event/:id', events.save);
+
+app.listen(8080);
+console.log('Listening on port 8080...');  
