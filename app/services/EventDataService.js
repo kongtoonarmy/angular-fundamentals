4b60f86ac2e7ea87ef62e2619bc88cbc709e8af4
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2016-01-14 21:01:48 +0700

    Saving Data with $resource (Chapter 3.10)

--- app/services/EventDataService.js
+++ app/services/EventDataService.js
@@ -1,11 +1,21 @@
 eventsApp.factory('eventData', function($resource) {
+
+	var resource = $resource('/data/event/:id', {id:'@id'}, {
+		"getAll": {
+			method: "GET",
+			isArray: true,
+			params: {
+				something: "foo"
+			}
+		}
+	});
 	return {
 		getEvent: function () {
-			/*return $http({ 
-				method: 'GET', 
-				url: '/data/event/1'
-			});*/
-			return $resource('/data/event/:id', {id:'@id'}).get({id:1});
-		}	
+			return resource.get({id:1});
+		},
+		save: function(event) {
+			event.id = 999;
+			return resource.save(event);
+		}	 
 	};
 }); 
