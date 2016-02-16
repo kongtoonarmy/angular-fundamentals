a9e70822e259eadde5f363867b898fe06a6a589e
kongtoonarmy <kongtoonarmy@hotmail.com>
2016-02-16 13:58:03 +0700

    Using Angular's $parse Service (Chapter 3.14)

--- app/controllers/CompileSampleController.js
+++ app/controllers/CompileSampleController.js
@@ -1,11 +1,28 @@
 'use strict';
 
-eventsApp.controller('CompileSampleController', function CompileSampleController($scope, $compile) {
+eventsApp.controller('CompileSampleController', function CompileSampleController($scope, $compile, $parse) {
 	
-	$scope.appendDivToElement = function(markup) {
+	var fn = $parse('1 + 2')
+	console.log(fn());
+
+	var getter = $parse('event.name');
+
+	var context1 = {event: {name: 'AngularJS Boot Camp'}};
+	var context2 = {event: {name: 'Code Camp', level: '20'}};
+
+	console.log(getter(context1));
+	console.log(getter(context2));
+	console.log(getter(context2, context1));
+
+	var setter = getter.assign;
+	setter(context2, 'Code Retreat');
+
+	console.log(context2.event.name);
+
+	$scope.appendDivToElement = function(markup) { 
 		return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
 		/*
-		 * Another way code
+		 * Alternative code
 		 *
 		var compile = $compile(markup); 
 		var content = compile($scope);
