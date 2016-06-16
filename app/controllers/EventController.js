1bca8d935acba28c0bbbf9461e3754b1a7d5ab3c
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2016-06-16 14:28:27 +0700

    Using Angular's $route service (Chapter 4.8)

--- app/controllers/EventController.js
+++ app/controllers/EventController.js
@@ -1,9 +1,17 @@
 'use strict';
 
-eventsApp.controller('EventController', function($scope, eventData, $anchorScroll, $routeParams) {
+eventsApp.controller('EventController', function($scope, eventData, $anchorScroll, $routeParams, $route) {
 
     $scope.sortorder = 'name',
     $scope.event = eventData.getEvent($routeParams.eventId);
+
+    console.log($route.current.pathParams.eventId);
+    console.log($route.current.pathParams.foo);
+    console.log($route.current.params.foo);
+
+    $scope.reload = function() {
+        $route.reload();
+    };
 
     $scope.upVoteSession = function(session) {
         session.upVoteCount++;
